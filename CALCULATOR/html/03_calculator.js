6f3f569d7c578126b593aab705cd2c5b35a6709c
Khushi Kumari <khushi16bce@gmail.com>
2025-11-20 23:13:46 +0530

    Upload calculator

---
+++ CALCULATOR/html/03_calculator.js
@@ -0,0 +1,25 @@
+let input = document.getElementById('inputbox');
+ let buttons = document.querySelectorAll('button');
+
+ let string = "";
+ let arr = Array.from(buttons); //make array of button//
+ arr.forEach(button => { 
+    button.addEventListener('click',(e)=>{
+if(e.target.innerHTML=='='){
+    string=eval(string);   //eval perform mathematical opration on ,(string) which is available in string//
+    input.value = string;
+}
+else if(e.target.innerHTML=='AC'){
+string = "";
+input.value = string;
+}
+else if(e.target.innerHTML=='DEL'){
+string = string.substring(0,string.length-1);
+input.value = string;
+}
+else{
+string += e.target.innerHTML;
+input.value = string;
+}
+    })
+ })
